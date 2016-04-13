6b66733977db348706ec658da45ea0d79bfac2c9
freewheel70 <lizhihong370@gmail.com>
2016-04-13 23:01:05 +0800

    correct the way to use flatmap

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ A simple app implementing RxJava
 
 
 <br/>
+To be continued ...
+<br/>
 <br/>
 I am learning RxJava, this is a testing app witnessing my learning process.  :-)
--- app/src/main/java/com/hong/app/rxjavatest/CityRequester.java
+++ app/src/main/java/com/hong/app/rxjavatest/CityRequester.java
@@ -16,45 +16,31 @@ public class CityRequester {
 
     public static void loadCityList(final Subscriber subscriber) {
 
-        Observable.create(new Observable.OnSubscribe<List<String>>() {
-            @Override
-            public void call(Subscriber<? super List<String>> subscriber) {
-                List<String> stringList = NetworkHelper.requestCityList();
-                subscriber.onNext(stringList);
-            }
-        })
+        Observable
+                .create(new Observable.OnSubscribe<List<String>>() {
+                    @Override
+                    public void call(Subscriber<? super List<String>> subscriber) {
+                        List<String> stringList = NetworkHelper.requestCityList();
+                        subscriber.onNext(stringList);
+                    }
+                })
                 .subscribeOn(Schedulers.io())
-                .subscribe(new Subscriber<List<String>>() {
+                .flatMap(new Func1<List<String>, Observable<String>>() {
+                    @Override
+                    public Observable<String> call(List<String> strings) {
+
+                        return Observable.from(strings);
+                    }
+                })
+                .map(new Func1<String, String>() {
 
                     @Override
-                    public void onCompleted() {
-
+                    public String call(String s) {
+                        return "City: " + s;
                     }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onNext(List<String> strings) {
-                        Observable.from(strings)
-                                .flatMap(new Func1<String, Observable<String>>() {
-                                    @Override
-                                    public Observable<String> call(final String s) {
-
-                                        return Observable.create(new Observable.OnSubscribe<String>() {
-                                            @Override
-                                            public void call(Subscriber<? super String> subscriber) {
-                                                subscriber.onNext("City: " + s);
-                                            }
-                                        });
-                                    }
-                                })
-                                .subscribeOn(Schedulers.io())
-                                .observeOn(AndroidSchedulers.mainThread())
-                                .subscribe(subscriber);
-                    }
-                });
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(subscriber);
+        
     }
 }
